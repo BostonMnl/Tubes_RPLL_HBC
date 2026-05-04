3e5fbfe761d432d2dd64c425fce8c0c9d3315277
bostonmnl <bostonmanalu688@gmail.com>
2026-05-04 15:08:33 +0700

    Repairing API: insentif, pinalty

--- Backend/API/routes/pinalti.routes.ts
+++ Backend/API/routes/pinalti.routes.ts
@@ -1,24 +1,25 @@
 import { Router } from 'express';
-import {
+import { 
   createPenalti,
-  getMyPenalti,
-  getPenalti,
-  getAllPenalti,
-  getPenaltiByUserId,
-  updatePenalti,
-  deletePenalti,
+  getMyPenalti, 
+  getPenaltiById, 
+  getAllPenalti, 
+  getPenaltiByUserId, 
+  updatePenalti, 
+  deletePenalti 
 } from '../controllers/pinalti.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { apiResponse } from '../middlewares/response.middleware';
+import { uploadImage } from '../utils/uploadUtils';
 
 const router = Router();
 
-router.post('/', authMiddleware(['admin'], ['manager', 'supervisor']), apiResponse(createPenalti));
+router.post('/', authMiddleware([], ['manager', 'supervisor']), uploadImage.single('gambar'), apiResponse(createPenalti));
 router.get('/me', authMiddleware(), apiResponse(getMyPenalti));
-router.get('/user/:userId', authMiddleware(['admin'], ['manager', 'supervisor']), apiResponse(getPenaltiByUserId));
-router.get('/:id', authMiddleware(), apiResponse(getPenalti));
+router.get('/user/:userId', authMiddleware([], ['manager', 'supervisor']), apiResponse(getPenaltiByUserId));
+router.get('/:id', authMiddleware(), apiResponse(getPenaltiById));
 router.get('/', authMiddleware(['admin']), apiResponse(getAllPenalti));
-router.put('/:id', authMiddleware(['admin'], ['manager', 'supervisor']), apiResponse(updatePenalti));
+router.put('/:id', authMiddleware([], ['manager', 'supervisor']), uploadImage.single('gambar'), apiResponse(updatePenalti));
 router.delete('/:id', authMiddleware(['admin']), apiResponse(deletePenalti));
 
 export default router;
